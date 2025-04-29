b941e8ab5643d44e79083f4eb95d497ef16ce254
Gustavo Queque <61836852+gqueque@users.noreply.github.com>
2025-04-28 22:20:30 -0300

    add scr/include/makefile/license

---
+++ src/cli.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "cli.h"
+
+void exibirMensagem() {
+    printf("Bem-vindo ao Projeto de PIF-SI!\n");
+}
---
+++ src/main.c
@@ -0,0 +1,6 @@
+#include "cli.h"
+
+int main() {
+    exibirMensagem();
+    return 0;
+}
